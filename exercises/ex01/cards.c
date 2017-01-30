3fe732b9e59ce7605e49667e99ba9ed7dc91a0f8
SeanCCarter <seancarter26@gmail.com>
2017-01-29 21:28:58 -0500

    Broke card counter into 3 functions.

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-	char card_name[3];
-	int count = 0;
-	int val;
-	while (card_name[0] != 'x') {
-		puts("Enter the card_name: ");
-		scanf("%2s", card_name);
-		val = 0;
-		switch(card_name[0]){
+int card_value(char* card_name){
+	int val = -1;
+	switch(card_name[0]){
 		case 'K':
 		case 'Q':
 		case 'J':
@@ -20,19 +13,38 @@ int main()
 			val = 11;
 			break;
 		case 'X':
-			continue;
+			break;
 		default:
 			val = atoi(card_name);
 			if ((val < 1) || (val > 10)){
-				puts("I don't understand that value!");
-				continue;
+				break;
 			}
+	}
+	return val;
+}
+
+int increment_count(int val, int count){
+	if ((val > 2) && (val < 7)){
+		count ++;
+	} else if (val == 10) {
+		count --;
+	}
+	return count;
+}
+
+int main()
+{
+	char card_name[3];
+	int count = 0;
+	int val;
+	while (card_name[0] != 'x') {
+		puts("Enter the card_name: ");
+		scanf("%2s", card_name);
+		val = card_value(card_name);
+		if (val == -1){
+			puts("I don't understand that value!");
 		}
-		if ((val > 2) && (val < 7)){
-			count ++;
-		} else if (val == 10) {
-			count --;
-		}
+		increment_count(val, count);
 		printf("Current count: %i\n", count);
 	}
 	return 0;
